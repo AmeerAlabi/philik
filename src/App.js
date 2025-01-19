da77593546e72a1d0fd7b7aefe0d344d9996b575
ameer alabi <ameeralabi7@gmail.com>
2025-01-19 22:04:16 +0100

    folder-structure and routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Team from './pages/Team';
+import About from './pages/About';
+import Programs from './pages/Programs';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <h1 className="text-3xl text-center text-red-600 font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+            <div className="overlay"></div> {/* Overlay for faded effect */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<Team />} />
+      </Routes>
+    </Router>
   );
 }
 
