2cfc8a0478688fa3145eab33ca4a396c4aaa9a0f
AmeerAlabi <146940613+AmeerAlabi@users.noreply.github.com>
2025-02-10 23:04:37 +0100

    Update Herop.jsx

--- src/components/Herop.jsx
+++ src/components/Herop.jsx
@@ -5,7 +5,7 @@ const Herop = () => {
     <div className="w-full max-w-[90%] sm:max-w-xl md:max-w-2xl lg:max-w-3xl md:mx-0 mx-auto
      my-6 px-8">
       {/* Heading */}
-      <h1 className="text-center sm:text-left bg-gradient-to-r from-[#000] to-[#F6A139] text-transparent bg-clip-text text-3xl sm:text-4xl md:text-5xl font-bold leading-tight 
+      <h1 className="text-center sm:text-left bg-gradient-to-r from-[#000] to-[#F6A139] text-transparent bg-clip-text text-3xl sm:text-4xl md:text-5xl font-bold leadin
       </h1>
 
       {/* Paragraph */}
