031eba5d3b756cdd21b0f7ed69a3280008a0c521
AmeerAlabi <146940613+AmeerAlabi@users.noreply.github.com>
2025-02-25 23:06:36 +0100

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -9,7 +9,7 @@ const Content = () => {
         <img
           src={offline}
           alt="Offline Learning"
-          className="w-[300px] h-[200px] md:w-[600px] md:h-[4
+          className="w-[300px] h-[200px] md:w-[600px] md
         />
       </div>
       
