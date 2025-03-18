edbbe7c9139c982a728905110595fd3b0732d252
AmeerAlabi <146940613+AmeerAlabi@users.noreply.github.com>
2025-03-18 22:43:04 +0100

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,7 +8,7 @@ const Dashboard = () => {
      
    
       {
-      <div className="w-fulĥhl my-px] md:w-[400px] fl flex-col just itsstart gap-[15px] text-center md:text-left">
+      <div className="w-fulĥhl my-px] md:w-[400px] fl flex-col just itssta gap-[15px] text-center md:text-left">
         <h1 className="font⁴ lg:text-[64px] leading-[1.2]">
         Interactiv
         </h
