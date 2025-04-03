6828b029f4415faf5316454b3597b259f79785f8
AmeerAlabi <146940613+AmeerAlabi@users.noreply.github.com>
2025-04-03 22:28:33 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
           Thank  messen sent succeully.
         </p>
      
-  
+
   retu
     <div className="my-12 mx-auto px-5 w-[85%] flex flex-col md:flex-r justify-center items-center md:items-start gap-10"
       {/* Heading */}
